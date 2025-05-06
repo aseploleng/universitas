43ece66a5974ba487a0fbafbbbd46748d706e616
aseploleng <willnjeng98@gmail.com>
2025-05-06 23:06:25 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -159,18 +159,10 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   
   // Create array of login URLs to rotate through
   const loginUrls = [
-    "https://t.ly/pablokiw",
-    "https://jali.me/panikaude",
-    "https://t.ly/pablokiw",
-    "https://jali.me/pintumasuk",
-    "https://t.ly/pablokiw",
-    "https://jali.me/panikaude",
-    "https://t.ly/pablokiw",
-    "https://jali.me/pintumasuk",
-    "https://t.ly/pablokiw",
-    "https://jali.me/panikaude",
-    "https://t.ly/pablokiw",
-    "https://jali.me/pintumasuk"
+    "https://jali.me/superuhuy",
+    "https://jali.me/bintanguhuy",
+    "https://jali.me/masteruhuy",
+    "https://jali.me/rumahuhuy"
   ];
   
   // Convert the array to JSON string for AMP state
@@ -589,13 +581,13 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   <!-- Header -->
   <header class="header">
     <div class="logo-container">
-      <a href="https://t.ly/pablokiw">
+      <a href="https://jali.me/superuhuy">
         <amp-img class="logo" src="https://pub-bc2ee8893baf416c8c23af0718d51fc3.r2.dev/slotgacorwin.gif" width="200" height="50" layout="fixed" alt="${siteName.toUpperCase()}"></amp-img>
       </a>
     </div>
     <nav class="main-nav">
-      <a href="https://t.ly/pablokiw" class="nav-link">Home</a>
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://jali.me/superuhuy" class="nav-link">Home</a>
+      <a href="https://jali.me/superuhuy" 
          class="login-btn login-btn-animated"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -627,7 +619,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
     
     <div class="site-slogan">Situs Slot Gacor Paling Aman Ga Pake Ribet!</div>
     <div class="action-buttons">
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://jali.me/superuhuy" 
          class="register-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -635,7 +627,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
              currentUrlIndex: (siteData.currentUrlIndex + 1) % siteData.loginUrls.length
            }
          })">Daftar ${siteName}</a>
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://jali.me/superuhuy" 
          class="login-block-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
